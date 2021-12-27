de816811f399a126ecc4a97a39e88e00e26b8840
임세현 <sh00710000@gmail.com>
2021-12-28 08:48:29 +0900

    Feat :: 코인 상세보기 페이지

---
+++ presentation/src/main/java/com/saehyun/efbit/feature/coin/CoinDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.efbit.feature.coin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoinDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_detail)
+    }
+}
