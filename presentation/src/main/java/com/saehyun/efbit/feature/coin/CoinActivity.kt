9791e9d2b63cd8889508f70670d813618cb7be72
임세현 <sh00710000@gmail.com>
2021-12-28 01:37:00 +0900

    Feat :: dataBindin처리

---
+++ presentation/src/main/java/com/saehyun/efbit/feature/coin/CoinActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.efbit.feature.coin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin)
+    }
+}
---
+++ presentation/src/main/java/com/saehyun/efbit/feature/exchange/ExchangeActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.efbit.feature.exchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExchangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange)
+    }
+}
