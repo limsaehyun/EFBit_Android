48a4d8fa91423baec0917f8c40e02cca7b933fb2
임세현 <sh00710000@gmail.com>
2021-12-28 01:37:30 +0900

    Fix :: base 상속받기

--- presentation/src/main/java/com/saehyun/efbit/feature/exchange/ExchangeActivity.kt
+++ presentation/src/main/java/com/saehyun/efbit/feature/exchange/ExchangeActivity.kt
@@ -2,10 +2,17 @@ package com.saehyun.efbit.feature.exchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.efbit.R
+import com.saehyun.efbit.databinding.ActivityExchangeBinding
 
-class ExchangeActivity : AppCompatActivity() {
+class ExchangeActivity : BaseActivity<ActivityExchangeBinding> (
+    R.layout.activity_exchange
+){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exchange)
+    }
+
+    override fun observeEvent() {
     }
 }
--- presentation/src/main/java/com/saehyun/efbit/feature/main/MainActivity.kt
+++ presentation/src/main/java/com/saehyun/efbit/feature/main/MainActivity.kt
@@ -1,16 +1,18 @@
-package com.saehyun.efbit.feature.coin
+package com.saehyun.efbit.feature.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.base.BaseActivity
 import com.saehyun.efbit.R
-import com.saehyun.efbit.databinding.ActivityCoinBinding
+import com.saehyun.efbit.databinding.ActivityMainBinding
 
-class CoinActivity : BaseActivity<ActivityCoinBinding>(
-    R.layout.coin_item
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
 ){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
 
     override fun observeEvent() {
