ca0eb3f75c225952e1fd6084951dbdc12377bae1
임세현 <sh00710000@gmail.com>
2021-12-28 19:26:37 +0900

    Feat :: toEntitiy

--- data/src/main/java/com/saehyun/data/remote/model/ExchangeResponse.kt
+++ data/src/main/java/com/saehyun/data/remote/model/ExchangeResponse.kt
@@ -1,5 +1,13 @@
 package com.saehyun.data.remote.model
 
+import com.saehyun.domain.entity.exchange.Exchange
+import com.saehyun.domain.entity.exchange.ExchangeEntity
+
 data class ExchangeResponse(
     val exchanges: List<Exchange>
 )
+
+fun ExchangeResponse.toEntity(): ExchangeEntity =
+    ExchangeEntity(
+        this.exchanges
+    )
