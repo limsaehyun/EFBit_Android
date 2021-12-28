24e863cc38ced985930e0c34e6f8f41adb29e921
임세현 <sh00710000@gmail.com>
2021-12-28 17:24:29 +0900

    Feat :; RetrofitModule

---
+++ presentation/src/main/java/com/saehyun/efbit/di/module/RetrofitModule.kt
@@ -0,0 +1,50 @@
+package com.saehyun.efbit.di.module
+
+import android.util.Log
+import com.saehyun.data.remote.ExchangeAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor { message -> Log.v("HTTP", message) }
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://www.cryptingup.com/api/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    @Singleton
+    @Provides
+    fun provideExchangeApi(
+        retrofit: Retrofit
+    ): ExchangeAPI =
+        retrofit.create(ExchangeAPI::class.java)
+}
