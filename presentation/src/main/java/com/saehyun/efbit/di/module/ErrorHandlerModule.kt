b913bc54489dc3307215f5ba2dd7c70579fcb6d9
임세현 <sh00710000@gmail.com>
2021-12-28 17:24:51 +0900

    Feat :: ErrorHandlerModule

---
+++ presentation/src/main/java/com/saehyun/efbit/di/module/ErrorHandlerModule.kt
@@ -0,0 +1,18 @@
+package com.saehyun.efbit.di.module
+
+import com.saehyun.data.handler.ExchangeErrorHandlerImpl
+import com.saehyun.domain.handler.ExchangeErrorHandler
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ErrorHandlerModule {
+
+    @Singleton
+    @Provides
+    fun errorHandler() : ExchangeErrorHandler = ExchangeErrorHandlerImpl()
+}
