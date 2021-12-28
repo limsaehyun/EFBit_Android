7ab411b82b6703f01adda7d1a883b30a5485f498
임세현 <sh00710000@gmail.com>
2021-12-28 17:24:23 +0900

    Feat :: MainViewModel

---
+++ presentation/src/main/java/com/saehyun/efbit/feature/main/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.saehyun.efbit.feature.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saehyun.domain.base.ErrorHandlerEntity
+import com.saehyun.domain.entity.exchange.ExchangeEntity
+import com.saehyun.domain.usecase.GetExchangeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getExchangeUseCase: GetExchangeUseCase
+) : ViewModel() {
+    val data = MutableLiveData<ExchangeEntity>()
+    val errorMesssage = MutableLiveData<String>()
+
+    fun getExchange() {
+        viewModelScope.launch {
+            try {
+                getExchangeUseCase.execute(Unit).collect {
+                    data.postValue(it)
+                }
+            } catch (e: ErrorHandlerEntity) {
+                errorMesssage.value = e.sendMessage
+            }
+
+        }
+    }
+}
