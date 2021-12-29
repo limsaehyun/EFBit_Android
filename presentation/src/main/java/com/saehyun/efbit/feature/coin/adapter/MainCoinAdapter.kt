b6e853777f2b3c5788c0e38f6a245bf49f3274d3
임세현 <sh00710000@gmail.com>
2021-12-29 15:54:17 +0900

    Feat :: MainCoinAdapter

---
+++ presentation/src/main/java/com/saehyun/efbit/feature/coin/adapter/MainCoinAdapter.kt
@@ -0,0 +1,59 @@
+package com.saehyun.efbit.feature.coin.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.saehyun.domain.entity.exchange.Exchange
+import com.saehyun.efbit.R
+import com.saehyun.efbit.databinding.ExchangeViewBinding
+
+import android.net.Uri
+
+import android.content.Intent
+import android.util.Log
+import com.saehyun.domain.entity.coin.Market
+import com.saehyun.efbit.databinding.CoinViewBinding
+import com.saehyun.efbit.feature.coin.ui.CoinDetailActivity
+import java.lang.Math.round
+
+
+class MainCoinAdapter(
+    val context: Context,
+    val data: List<Market>
+    ) : RecyclerView.Adapter<MainCoinAdapter.Holder>() {
+
+    private lateinit var binding: CoinViewBinding
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        binding = DataBindingUtil.inflate<CoinViewBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.coin_view, parent, false
+        )
+        return Holder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val data = data.get(position)
+
+        binding.run {
+            tvCoinExchange.text = data.exchange_id
+            tvCoinName.text = data.base_asset
+            tvCoinPrice.text = "%.2f".format(data.price)
+            tvCoinPercent.text = "%.2f".format(data.change_24h)
+
+            holder.itemView.setOnClickListener {
+                holder.itemView.context.startActivity(Intent(context, CoinDetailActivity::class.java))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
