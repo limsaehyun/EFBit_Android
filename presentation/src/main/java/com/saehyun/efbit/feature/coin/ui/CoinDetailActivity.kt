93d42c43acd890841dcb4f9ceb7aea4a144dc2ce
임세현 <sh00710000@gmail.com>
2021-12-29 19:34:11 +0900

    Feat :: setData

--- presentation/src/main/java/com/saehyun/efbit/feature/coin/ui/CoinDetailActivity.kt
+++ presentation/src/main/java/com/saehyun/efbit/feature/coin/ui/CoinDetailActivity.kt
@@ -1,8 +1,12 @@
 package com.saehyun.efbit.feature.coin.ui
 
+import android.content.ContentValues.TAG
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.domain.entity.coin.Market
 import com.saehyun.efbit.R
 import com.saehyun.efbit.databinding.ActivityCoinDetailBinding
 
@@ -11,8 +15,31 @@ class CoinDetailActivity : BaseActivity<ActivityCoinDetailBinding> (
 ) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setData()
+
+        binding.imageView.setOnClickListener {
+            finish()
+        }
+
     }
 
     override fun observeEvent() {
+    }
+
+    private fun setData() {
+        val data = intent.getSerializableExtra("data") as Market
+
+        binding.run {
+            tvTitle.text = data.base_asset
+            tvCoinSubName.text = data.symbol
+
+            textView15.text = data.price.toString()
+            tvCoinPercent.text = data.volume_24h.toString()
+
+            tvCoin24HChanged.text = "%.2f".format(data.change_24h)
+
+            tvCoinValue.text = data.volume_24h.toString()
+        }
     }
 }
