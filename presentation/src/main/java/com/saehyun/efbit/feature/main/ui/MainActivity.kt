532b8c402cd686a250e48c3583696420b49dd6e5
임세현 <sh00710000@gmail.com>
2021-12-29 15:54:45 +0900

    Feat :: 코인 리스트 미리보기

--- presentation/src/main/java/com/saehyun/efbit/feature/main/ui/MainActivity.kt
+++ presentation/src/main/java/com/saehyun/efbit/feature/main/ui/MainActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.domain.entity.coin.CoinEntity
+import com.saehyun.domain.entity.coin.Market
 import com.saehyun.domain.entity.exchange.Exchange
 import com.saehyun.domain.entity.exchange.ExchangeEntity
 import com.saehyun.efbit.R
 import com.saehyun.efbit.databinding.ActivityMainBinding
+import com.saehyun.efbit.databinding.CoinViewBinding
+import com.saehyun.efbit.feature.coin.adapter.MainCoinAdapter
+import com.saehyun.efbit.feature.coin.ui.CoinActivity
+import com.saehyun.efbit.feature.coin.viewmodel.CoinViewModel
 import com.saehyun.efbit.feature.exchange.ExchangeActivity
-import com.saehyun.efbit.feature.main.adapter.MainCoinAdapter
+import com.saehyun.efbit.feature.main.adapter.MainExchangeAdapter
 import com.saehyun.efbit.feature.main.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -21,9 +28,11 @@ class MainActivity @Inject constructor(
     R.layout.activity_main
 ){
 
-    private val data = arrayListOf<Exchange>()
+    private val exchangeData = arrayListOf<Exchange>()
+    private val coinData = arrayListOf<Market>()
 
     private val mainViewModel: MainViewModel by viewModels()
+    private val coinViewModel: CoinViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +42,14 @@ class MainActivity @Inject constructor(
         setRecyclerview()
 
         mainViewModel.getExchange()
+        coinViewModel.getCoin()
 
         binding.tvExchangeMore.setOnClickListener {
             startActivity(Intent(applicationContext, ExchangeActivity::class.java))
+        }
+
+        binding.tvMainCoinMore.setOnClickListener {
+            startActivity(Intent(applicationContext, CoinActivity::class.java))
         }
     }
 
@@ -48,26 +62,48 @@ class MainActivity @Inject constructor(
     }
 
     private fun setRecyclerview() {
-        binding.run {
-            binding.rvMainExchange.layoutManager = GridLayoutManager(this@MainActivity, 2)
-            binding.rvMainExchange.setHasFixedSize(true)
-            binding.rvMainExchange.adapter= MainCoinAdapter(applicationContext, data)
+        binding.rvMainExchange.run {
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
+            setHasFixedSize(true)
+            adapter= MainExchangeAdapter(applicationContext, exchangeData)
+        }
+
+        binding.rvCoinMain.run {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter= MainCoinAdapter(applicationContext, coinData)
         }
     }
 
-    private fun setRecyclerviewData(data: ExchangeEntity) {
+    private fun setExchangeRecyclerviewData(data: ExchangeEntity) {
         for(i: Int in 1..4) {
-            this.data.add(data.exchanges.get(i))
+            exchangeData.add(data.exchanges.get(i))
         }
         binding.rvMainExchange.adapter?.notifyDataSetChanged()
+    }
+
+    private fun setCoinRecyclerviewData(data: CoinEntity) {
+        for(i: Int in 1..4) {
+            coinData.add(data.markets.get(i))
+        }
+        binding.rvCoinMain.adapter?.notifyDataSetChanged()
     }
 
     override fun observeEvent() {
         mainViewModel.run {
             data.observe(this@MainActivity, {
-                setRecyclerviewData(it)
+                setExchangeRecyclerviewData(it)
             })
 
+            errorMesssage.observe(this@MainActivity, {
+                showToast(it.toString())
+            })
+        }
+
+        coinViewModel.run {
+            data.observe(this@MainActivity, {
+                setCoinRecyclerviewData(it)
+            })
             errorMesssage.observe(this@MainActivity, {
                 showToast(it.toString())
             })
