c05ccdeeb266f7a9c2424b14b00b5496c35aa5a0
임세현 <sh00710000@gmail.com>
2021-12-29 15:51:15 +0900

    Feat :: CoinResponse

---
+++ data/src/main/java/com/saehyun/data/remote/model/CoinResponse.kt
@@ -0,0 +1,14 @@
+package com.saehyun.data.remote.model
+
+import com.saehyun.domain.entity.coin.CoinEntity
+import com.saehyun.domain.entity.coin.Market
+import com.saehyun.domain.entity.exchange.ExchangeEntity
+
+data class CoinResponse(
+    val markets: List<Market>
+)
+
+fun CoinResponse.toEntity(): CoinEntity =
+    CoinEntity(
+        this.markets
+    )
