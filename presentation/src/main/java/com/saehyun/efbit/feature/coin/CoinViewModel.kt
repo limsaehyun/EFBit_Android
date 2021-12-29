10877e585c0a8b3985ab6355431a60c2148569c2
임세현 <sh00710000@gmail.com>
2021-12-29 11:57:49 +0900

    Feat :: CoinViewModel

---
+++ presentation/src/main/java/com/saehyun/efbit/feature/coin/CoinViewModel.kt
@@ -0,0 +1,33 @@
+package com.saehyun.efbit.feature.coin
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saehyun.domain.base.ErrorHandlerEntity
+import com.saehyun.domain.entity.exchange.ExchangeEntity
+import com.saehyun.domain.usecase.GetExchangeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinViewModel @Inject constructor(
+    private val getExchangeUseCase: GetExchangeUseCase
+) : ViewModel() {
+    val data = MutableLiveData<ExchangeEntity>()
+    val errorMesssage = MutableLiveData<String>()
+
+    fun getExchange() {
+        viewModelScope.launch {
+            try {
+                getExchangeUseCase.execute(Unit).collect {
+                    data.value = it
+                }
+            } catch (e: ErrorHandlerEntity) {
+                errorMesssage.value = e.sendMessage
+            }
+
+        }
+    }
+}
