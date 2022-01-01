ec95b370007cea8ba516979d44d24c1587ee4d01
임세현 <sh00710000@gmail.com>
2022-01-01 12:34:14 +0900

    Feat :: ExchangeDatabase

---
+++ data/src/main/java/com/saehyun/data/local/database/ExchangeDatabase.kt
@@ -0,0 +1,15 @@
+package com.saehyun.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.saehyun.data.local.dao.ExchangeDao
+import com.saehyun.data.local.entitiy.ExchangeRoomEntity
+
+@Database(
+    entities = [ExchangeRoomEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class ExchangeDatabase: RoomDatabase() {
+    abstract fun exchangeDao() : ExchangeDao
+}
