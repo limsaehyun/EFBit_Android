980b5410e728dc28addffdb0a07b0648a1633e1b
임세현 <sh00710000@gmail.com>
2022-01-01 12:35:16 +0900

    Fix :: Room 데이터 활용

--- data/src/main/java/com/saehyun/data/repository/ExchangeRepositoryImpl.kt
+++ data/src/main/java/com/saehyun/data/repository/ExchangeRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.saehyun.data.repository
 
+import com.saehyun.data.local.datasource.LocalExchangeDataSource
+import com.saehyun.data.local.entitiy.toEntity
 import com.saehyun.data.remote.datasource.exchange.RemoteExchangeDataSource
 import com.saehyun.data.remote.model.toEntity
+import com.saehyun.data.util.OfflineCacheUtil
 import com.saehyun.domain.entity.exchange.ExchangeEntity
 import com.saehyun.domain.repository.ExchangesRepository
 import kotlinx.coroutines.flow.Flow
@@ -9,11 +12,14 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ExchangeRepositoryImpl @Inject constructor(
-    private val remoteExchangeDataSource: RemoteExchangeDataSource
+    private val remoteExchangeDataSource: RemoteExchangeDataSource,
+    private val localExchangeDataSource: LocalExchangeDataSource
 ) : ExchangesRepository {
 
     override suspend fun getExchange(): Flow<ExchangeEntity> =
-        flow {
-            emit(remoteExchangeDataSource.getExchange().toEntity())
-        }
+        OfflineCacheUtil<ExchangeEntity>
+            .remoteData { remoteExchangeDataSource.getExchange().toEntity() }
+            .localData { localExchangeDataSource.getExchange().toEntity() }
+            .doOnNeedRefresh { localExchangeDataSource.updateExchange() }
+            .createFlow()
 }
