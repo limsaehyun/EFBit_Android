15452bc412d186346a3d50fd3ff7179e2bc89f0e
임세현 <sh00710000@gmail.com>
2022-01-03 15:04:59 +0900

    Fix :: List 형식으로 주고받기

--- data/src/main/java/com/saehyun/data/local/datasource/LocalExchangeDataSource.kt
+++ data/src/main/java/com/saehyun/data/local/datasource/LocalExchangeDataSource.kt
@@ -1,12 +1,12 @@
 package com.saehyun.data.local.datasource
 
 import com.saehyun.data.local.entitiy.ExchangeRoomEntity
+import com.saehyun.domain.entity.exchange.ExchangeEntity
 
 interface LocalExchangeDataSource {
+    suspend fun getExchange(): ExchangeEntity
 
-    suspend fun getExchange(): ExchangeRoomEntity
+    suspend fun insertExchange(exchangeRoomEntity: List<ExchangeRoomEntity>)
 
-    suspend fun insertExchange(exchangeRoomEntity: ExchangeRoomEntity)
-
-    suspend fun updateExchange(exchangeRoomEntity: ExchangeRoomEntity)
+    suspend fun updateExchange(exchangeRoomEntity: List<ExchangeRoomEntity>)
 }
--- data/src/main/java/com/saehyun/data/local/datasource/LocalExchangeDataSourceImpl.kt
+++ data/src/main/java/com/saehyun/data/local/datasource/LocalExchangeDataSourceImpl.kt
@@ -2,19 +2,21 @@ package com.saehyun.data.local.datasource
 
 import com.saehyun.data.local.dao.ExchangeDao
 import com.saehyun.data.local.entitiy.ExchangeRoomEntity
+import com.saehyun.data.local.entitiy.toEntity
+import com.saehyun.domain.entity.exchange.ExchangeEntity
 import javax.inject.Inject
 
 class LocalExchangeDataSourceImpl @Inject constructor(
     private val exchangeDao: ExchangeDao
 ) : LocalExchangeDataSource {
 
-    override suspend fun getExchange() : ExchangeRoomEntity =
-        exchangeDao.getExchange()
+    override suspend fun getExchange() : ExchangeEntity =
+        exchangeDao.getExchange().toEntity()
 
-    override suspend fun insertExchange(exchangeRoomEntity: ExchangeRoomEntity) =
+    override suspend fun insertExchange(exchangeRoomEntity: List<ExchangeRoomEntity>) =
         exchangeDao.insertExchange(exchangeRoomEntity)
 
-    override suspend fun updateExchange(exchangeRoomEntity: ExchangeRoomEntity) {
+    override suspend fun updateExchange(exchangeRoomEntity: List<ExchangeRoomEntity>) {
         exchangeDao.updateExchange(exchangeRoomEntity)
     }
 
