8309ea448246dcaca1ab41f461f47f0c1c9e1070
임세현 <sh00710000@gmail.com>
2022-01-03 15:04:03 +0900

    Fix :: Query문 수정

--- data/src/main/java/com/saehyun/data/local/dao/ExchangeDao.kt
+++ data/src/main/java/com/saehyun/data/local/dao/ExchangeDao.kt
@@ -6,17 +6,17 @@ import com.saehyun.data.local.entitiy.ExchangeRoomEntity
 @Dao
 interface ExchangeDao {
 
-    @Query("SELECT * FROM ExchangeRoomEntity")
-    suspend fun getExchange(): ExchangeRoomEntity
+    @Query("SELECT * FROM exchange_room")
+    suspend fun getExchange(): List<ExchangeRoomEntity>
 
-    @Query("DELETE FROM ExchangeRoomEntity")
+    @Query("DELETE FROM exchange_room")
     suspend fun deleteExchange()
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertExchange(exchangeRoomEntity: ExchangeRoomEntity)
+    suspend fun insertExchange(exchangeRoomEntity: List<ExchangeRoomEntity>)
 
     @Transaction
-    suspend fun updateExchange(repo: ExchangeRoomEntity) {
+    suspend fun updateExchange(repo: List<ExchangeRoomEntity>) {
         deleteExchange()
         insertExchange(repo)
     }
