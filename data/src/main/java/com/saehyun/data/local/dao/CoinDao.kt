ce8bf14b20c1e31f14b14baf38994efb43a34add
임세현 <sh00710000@gmail.com>
2022-01-03 15:35:46 +0900

    Feat :: CoinDao

---
+++ data/src/main/java/com/saehyun/data/local/dao/CoinDao.kt
@@ -0,0 +1,27 @@
+package com.saehyun.data.local.dao
+
+import androidx.room.*
+import com.saehyun.data.local.entitiy.CoinRoomEntity
+import com.saehyun.data.local.entitiy.ExchangeRoomEntity
+
+@Dao
+interface CoinDao {
+
+    @Query("SELECT * FROM coin_room")
+    suspend fun getCoin(): List<CoinRoomEntity>
+
+    @Query("DELETE FROM coin_room")
+    suspend fun deleteCoin()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCoin(coinRoomEntity: List<CoinRoomEntity>)
+
+    @Transaction
+    suspend fun updateExchange(repo: List<CoinRoomEntity>) {
+        deleteCoin()
+        insertCoin(repo)
+    }
+
+
+
+}
