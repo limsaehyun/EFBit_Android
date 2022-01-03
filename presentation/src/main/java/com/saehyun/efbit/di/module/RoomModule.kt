a480676c8b84740e1caf22215b47338bc1037be6
임세현 <sh00710000@gmail.com>
2022-01-03 15:36:56 +0900

    Feat :: Coin관련 Module

--- presentation/src/main/java/com/saehyun/efbit/di/module/RoomModule.kt
+++ presentation/src/main/java/com/saehyun/efbit/di/module/RoomModule.kt
@@ -2,7 +2,9 @@ package com.saehyun.efbit.di.module
 
 import android.content.Context
 import androidx.room.Room
+import com.saehyun.data.local.dao.CoinDao
 import com.saehyun.data.local.dao.ExchangeDao
+import com.saehyun.data.local.database.CoinDatabase
 import com.saehyun.data.local.database.ExchangeDatabase
 import dagger.Module
 import dagger.Provides
@@ -30,4 +32,18 @@ object RoomModule {
         exchangeDatabase: ExchangeDatabase
     ): ExchangeDao = exchangeDatabase.exchangeDao()
 
+    @Provides
+    fun provideCoinDatabase(
+        @ApplicationContext context: Context
+    ): CoinDatabase = Room
+        .databaseBuilder(
+            context,
+            CoinDatabase::class.java,
+            "coin"
+        ).build()
+
+    @Provides
+    fun provideCoinDao(
+        coinDatabase: CoinDatabase
+    ): CoinDao = coinDatabase.coinDao()
 }
--- presentation/src/main/java/com/saehyun/efbit/di/module/coin/CoinDataSourceModule.kt
+++ presentation/src/main/java/com/saehyun/efbit/di/module/coin/CoinDataSourceModule.kt
@@ -1,5 +1,9 @@
 package com.saehyun.efbit.di.module.coin
 
+import com.saehyun.data.local.dao.CoinDao
+import com.saehyun.data.local.datasource.LocalCoinDataSource
+import com.saehyun.data.local.datasource.LocalCoinDataSourceImpl
+import com.saehyun.data.local.datasource.LocalExchangeDataSourceImpl
 import com.saehyun.data.remote.datasource.coin.RemoteCoinDataSource
 import com.saehyun.data.remote.datasource.coin.RemoteCoinDataSourceImpl
 import com.saehyun.data.remote.network.CoinAPI
@@ -19,4 +23,10 @@ object CoinDataSourceModule {
         coinAPI: CoinAPI,
         errorHandler: CoinErrorHandler
     ): RemoteCoinDataSource = RemoteCoinDataSourceImpl(coinAPI, errorHandler)
+
+    @Singleton
+    @Provides
+    fun provideLocalCoinDataSource(
+        coinDao: CoinDao
+    ) : LocalCoinDataSource = LocalCoinDataSourceImpl(coinDao)
 }
